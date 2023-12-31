18adc26ec4753dfc1dc3e2d297f120e29821a619
Serkan <224212041@ogrenci.amasya.edu.tr>
2023-12-31 22:38:11 +0300

    Profil Görüntüleme
    Profil görüntüleme oluşturuldu.

---
+++ profil.php
@@ -0,0 +1,50 @@
+<center>
+
+<a href = "index.php"> <h1> Forum Sitesi PHP <> Serkan Armutlu </h1> </a>
+
+<a href = " "> Üye Ol </a> veya <a href = " "> Giriş Yap </a>
+
+<br>
+<a href = "profil.php?yin=serkan"> Profilime Git </a>
+<a href = "profil.php?q=cikis"> Çıkış Yap </a>
+<br><br>
+<h2>Serkan Mutlu</h2>
+<strong> Gmail: </strong> unknown@gmail.com
+<hr>
+
+<table border = "1" width = 100% >
+	<tr>
+		<td>
+			<strong> Yeni Açılan Konular: </strong>
+			<ul>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+			</ul>
+		</td>
+		<td>
+			<strong> Son Cevaplar: </strong><hr>
+			<ul>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+			</ul>
+		</td>
+	</tr>
+</table>
+
+</center>
