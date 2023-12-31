b6de313b5aa45c9b8d16b3b30e9f89e733438b16
Serkan <224212041@ogrenci.amasya.edu.tr>
2023-12-31 22:04:39 +0300

    Konu Okuma
    Konu okuma oluşturuldu.

---
+++ konu.php
@@ -0,0 +1,67 @@
+<center>
+
+<a href = "index.php"> <h1> Forum Sitesi PHP <> Serkan Armutlu </h1> </a>
+
+<a href = " "> Üye Ol </a> veya <a href = " "> Giriş Yap </a>
+
+<br>
+<a href = "profil.php?yin=serkan"> Profilime Git </a>
+<a href = "profil.php?q=cikis"> Çıkış Yap </a>
+
+<br> <br>
+
+<h2> Konu Başlığı </h2>
+
+<strong> Konu Sahibi: </strong> <a href= "profil.php?yin=kemal"> Kemal Dokumacı </a>
+
+<p>
+    Konu Mesajlar
+</p>
+
+<hr>
+
+<h3> Yorumlar: </h3>
+
+<a href= "profil.php?yin=serkan"> <strong> Serkan Mutlu </strong> </a> <br>
+
+<p>
+    Lorem ipsum dolor sit amet.
+</p>
+
+<small> Tarih: </small>
+
+<hr>
+
+<a href= "profil.php?yin=serkan"> <strong> Serkan Mutlu </strong> </a> <br>
+
+<p>
+    Lorem ipsum dolor sit amet.
+</p>
+
+<small> Tarih: </small>
+
+<hr>
+
+<a href= "profil.php?yin=serkan"> <strong> Serkan Mutlu </strong> </a> <br>
+
+<p>
+    Lorem ipsum dolor sit amet.
+</p>
+
+<small> Tarih: </small>
+
+<h4> Yorum Yap: </h4>
+
+<form action = " " method = "post">
+    
+<textarea name = "yorum" cols = "30" rows = "10"> </textarea>
+
+<br> <br>
+
+<input type = "submit" value = "Yorum Yap">
+
+</form>
+
+Yorum yapabilmek için <a href = "uyelik.php"> Giriş Yap </a> veya <a href = "uyelik.php?q=kayit"> Kayıt Ol </a>
+
+</center>
