2d975e312a8f3325491315e532f750b3e0f14c03
Serkan <224212041@ogrenci.amasya.edu.tr>
2023-12-31 22:24:48 +0300

    Admin Paneli
    Admin paneli oluşturuldu.

---
+++ admin_paneli.php
@@ -0,0 +1,44 @@
+<center>
+
+<a href = "index.php"> <h1> Forum Sitesi PHP <> Serkan Armutlu </h1> </a>
+
+<a href = " "> Üye Ol </a> veya <a href = " "> Giriş Yap </a>
+
+<br>
+<a href = "profil.php?yin=serkan"> Profilime Git </a>
+<a href = "profil.php?q=cikis"> Çıkış Yap </a>
+
+<br> <br>
+
+<h2> Admin Paneli </h2>
+
+<hr>
+
+<form action = " " method = "post">
+
+<h3> Kategori Ekle: </h3>
+    
+<input type = "text" name = "kategori"> 
+
+<br>
+
+<input type = "submit" value = "Kategori Oluştur">
+
+</form>
+
+<hr>
+
+<ol>
+    <li><a href=" "> Kategorimiz </a></li>
+    <li><a href=" "> Kategorimiz </a></li>
+    <li><a href=" "> Kategorimiz </a></li>
+    <li><a href=" "> Kategorimiz </a></li>
+    <li><a href=" "> Kategorimiz </a></li>
+    <li><a href=" "> Kategorimiz </a></li>
+    <li><a href=" "> Kategorimiz </a></li>
+    <li><a href=" "> Kategorimiz </a></li>
+    <li><a href=" "> Kategorimiz </a></li>
+    <li><a href=" "> Kategorimiz </a></li>
+</ol>
+
+</center>
