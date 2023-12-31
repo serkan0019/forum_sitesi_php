fb2f06a56e2041e56feea561c08414627a2e1c15
Serkan <224212041@ogrenci.amasya.edu.tr>
2023-12-31 22:44:15 +0300

    Alt Kategori
    Alt kategori oluşturuldu.

---
+++ kategori.php
@@ -0,0 +1,30 @@
+<center>
+
+<a href = "index.php"> <h1> Forum Sitesi PHP <> Serkan Armutlu </h1> </a>
+
+<a href = " "> Üye Ol </a> veya <a href = " "> Giriş Yap </a>
+
+<br>
+<a href = "profil.php?yin=serkan"> Profilime Git </a>
+<a href = "profil.php?q=cikis"> Çıkış Yap </a>
+
+<br> <br>
+
+<h2> Python Dersleri </h2>
+
+<ul>
+				<li><a href=" "> Kategori İçindeki Başlıklar </a></li>
+				<li><a href=" "> Kategori İçindeki Başlıklar </a></li>
+				<li><a href=" "> Kategori İçindeki Başlıklar </a></li>
+				<li><a href=" "> Kategori İçindeki Başlıklar </a></li>
+				<li><a href=" "> Kategori İçindeki Başlıklar </a></li>
+				<li><a href=" "> Kategori İçindeki Başlıklar </a></li>
+				<li><a href=" "> Kategori İçindeki Başlıklar </a></li>
+				<li><a href=" "> Kategori İçindeki Başlıklar </a></li>
+				<li><a href=" "> Kategori İçindeki Başlıklar </a></li>
+				<li><a href=" "> Kategori İçindeki Başlıklar </a></li>
+</ul>
+
+</form>
+
+</center>
