89ecb7bc9c4a7c4c02d42cd4588763b32078b2c0
Serkan <224212041@ogrenci.amasya.edu.tr>
2023-12-31 22:14:35 +0300

    Konu Aç
    Konu açma oluşturuldu.

---
+++ konu_ac.php
@@ -0,0 +1,33 @@
+<center>
+
+<a href = "index.php"> <h1> Forum Sitesi PHP <> Serkan Armutlu </h1> </a>
+
+<a href = " "> Üye Ol </a> veya <a href = " "> Giriş Yap </a>
+
+<br>
+<a href = "profil.php?yin=serkan"> Profilime Git </a>
+<a href = "profil.php?q=cikis"> Çıkış Yap </a>
+
+<br> <br>
+
+<h2> Konu Paylaşma </h2>
+
+<form action = " " method = "post">
+
+<strong> Konu Adı: </strong>
+
+<input type = "text" name = "ad"> <br>
+
+<strong> Konu Mesajı: </strong>
+    
+<textarea name = "yorum" cols = "30" rows = "10"> </textarea>
+
+<br>
+
+<input type = "submit" value = "Konuyu Aç">
+
+</form>
+
+Konu paylaşabilmek için <a href = "uyelik.php"> Giriş Yap </a> veya <a href = "uyelik.php?q=kayit"> Kayıt Ol </a>
+
+</center>
