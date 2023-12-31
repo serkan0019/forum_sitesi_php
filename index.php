aec3b93bf54ebf55fc853dc2ae1809d4f61136a7
Serkan <224212041@ogrenci.amasya.edu.tr>
2023-12-31 21:43:26 +0300

    Ana Sayfa <> v_1
    Ana sayfa güncellendi.

--- index.php
+++ index.php
@@ -2,4 +2,61 @@
 
 <h1> Forum Sitesi PHP <> Serkan Armutlu </h1>
 
+<a href = " " > Üye Ol </a> Veya <a href = " " > Giriş Yap </a>
+
+<br> <br>
+
+<table border = "1" >
+	<tr>
+		<td>
+			<strong> Yeni Açılan Konular: </strong><hr>
+			<ul>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+			</ul>
+		</td>
+		<td>
+			<strong> Son Cevaplar: </strong><hr>
+			<ul>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+			</ul>
+		</td>
+	</tr>
+	<tr>
+		<td colspan = "2" >
+			<h2> Kategoriler: </h2>
+
+			<ul>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+				<li><a href=""> Konu </a></li>
+			</ul>
+		</td>
+	</tr>
+</table>
+
 </center>
