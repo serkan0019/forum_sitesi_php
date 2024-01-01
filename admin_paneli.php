5a42c7a8c4d9ab6a42009b3918017a10c476452a
Serkan <224212041@ogrenci.amasya.edu.tr>
2024-01-01 15:54:09 +0300

    Admin Paneli <> v1
    Veri tabanına kategori adında sütun ekledim. Admin panelinden kategori adı girip kategori ekle dediğim de veri tabanına kategoriyi ekliyor.
    
    ---------------------------------------
    
    Veri tabanında üye onay sütunu var. Bu sütun kullanıcının onayı var mı yani admin mi? Değil mi? Diye kontrol ediyor. Eğer adminse kategori ekleyebilir, yani sayfaya erişebiliyor. Değil ise sayfaya erişemiyor.

--- admin_paneli.php
+++ admin_paneli.php
@@ -1,12 +1,18 @@
+<?php
+session_start();
+include 'ayar.php';
+include 'ukas.php';
+include 'fonksiyon.php';
+
+if (!@$_SESSION['uye_onay'] == 1 )
+{
+    echo '<center><h1>Yönetici değilsiniz! Panele erişemezsiniz!</h1><center>';
+    exit;
+}
+?>
+
 <center>
-
-<a href = "index.php"> <h1> Forum Sitesi PHP <> Serkan Armutlu </h1> </a>
-
-<a href = " "> Üye Ol </a> veya <a href = " "> Giriş Yap </a>
-
-<br>
-<a href = "profil.php?yin=serkan"> Profilime Git </a>
-<a href = "profil.php?q=cikis"> Çıkış Yap </a>
+<?php include 'ust_bilgi.php'; ?>
 
 <br> <br>
 
@@ -18,27 +24,51 @@
 
 <h3> Kategori Ekle: </h3>
     
-<input type = "text" name = "kategori"> 
+<input type = "text" name = "kategori">
 
 <br>
 
 <input type = "submit" value = "Kategori Oluştur">
+<?php
+if ($_POST) {
+    $kategori = $_POST["kategori"];
+    $kategori_link = permalink($kategori);
 
+$dataAdd = $db -> prepare("INSERT INTO kategoriler SET
+    k_kategori=?,
+    k_kategori_link=?
+");
+$dataAdd -> execute([
+    $kategori,
+    $kategori_link
+]);
+
+if ( $dataAdd ) {
+    echo '<p class="alert alert-success">Kategori Eklendi!</p>';
+    
+    header("REFRESH:1;URL=admin_paneli.php");
+} else {
+    echo '<p class="alert alert-danger">Kategori eklenirken sıkıntı oldu!</p>';
+    
+    header("REFRESH:1;URL=admin_paneli.php");
+}
+}
+?>
 </form>
 
 <hr>
 
 <ol>
-    <li><a href=" "> Kategorimiz </a></li>
-    <li><a href=" "> Kategorimiz </a></li>
-    <li><a href=" "> Kategorimiz </a></li>
-    <li><a href=" "> Kategorimiz </a></li>
-    <li><a href=" "> Kategorimiz </a></li>
-    <li><a href=" "> Kategorimiz </a></li>
-    <li><a href=" "> Kategorimiz </a></li>
-    <li><a href=" "> Kategorimiz </a></li>
-    <li><a href=" "> Kategorimiz </a></li>
-    <li><a href=" "> Kategorimiz </a></li>
+<?php
+
+$dataList = $db -> prepare("SELECT * FROM kategoriler ");
+$dataList -> execute();
+$dataList = $dataList -> fetchALL(PDO::FETCH_ASSOC);
+
+foreach($dataList as $row){
+    echo '<li><a href = "kategori.php?q='.$row["k_kategori_link"].'">'.$row["k_kategori"].'</a></li>';
+}
+?>
 </ol>
 
 </center>
