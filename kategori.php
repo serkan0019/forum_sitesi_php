ebbc93adb92f6fc6c632e7d62f3ceb304e098bf9
Serkan <224212041@ogrenci.amasya.edu.tr>
2024-01-01 15:50:27 +0300

    Kategori <> v1
    Veri tabanında ki tablo da admin panelinde eklemiş olduğum kategori verilerini çektim.

--- kategori.php
+++ kategori.php
@@ -1,16 +1,24 @@
+<?php
+session_start();
+include 'ayar.php';
+include 'ukas.php';
+include 'fonksiyon.php';
+
+$q = @$_GET["q"];
+
+$data = $db -> prepare("SELECT * FROM kategoriler WHERE k_kategori_link=?
+");
+$data -> execute([
+  $q
+]);
+$_data = $data -> fetch(PDO::FETCH_ASSOC);
+?>
 <center>
-
-<a href = "index.php"> <h1> Forum Sitesi PHP <> Serkan Armutlu </h1> </a>
-
-<a href = " "> Üye Ol </a> veya <a href = " "> Giriş Yap </a>
-
-<br>
-<a href = "profil.php?yin=serkan"> Profilime Git </a>
-<a href = "profil.php?q=cikis"> Çıkış Yap </a>
+<?php include 'ust_bilgi.php'; ?>
 
 <br> <br>
 
-<h2> Python Dersleri </h2>
+<h2><?php echo $_data["k_kategori"]; ?></h2>
 
 <ul>
 				<li><a href=" "> Kategori İçindeki Başlıklar </a></li>
