4d877837e7dd86ab28b33b14e6c3a0dcf7f808a3
Serkan <224212041@ogrenci.amasya.edu.tr>
2024-01-09 22:07:18 +0300

    Profil <> v1_2
    Üye kullanıcının profil görüntülenmesi eklendi. Kullanıcının açtığı konular ve yaptığı yorumlar ( Konuların adı görüntüleniyor. ) eklendi.

--- profil.php
+++ profil.php
@@ -3,13 +3,23 @@ session_start();
 include 'ayar.php';
 include 'ukas.php';
 include 'fonksiyon.php';
+
+$kadi = @$_GET["kadi"];
+
+$data = $db -> prepare("SELECT * FROM uyeler WHERE
+  uye_kadi=?
+");
+$data -> execute([
+  $kadi
+]);
+$_data = $data -> fetch(PDO::FETCH_ASSOC);
 ?>
 
 <center>
 <?php include 'ust_bilgi.php'; ?>
 <br><br>
-<h2>Serkan Mutlu</h2>
-<strong> Gmail: </strong> unknown@gmail.com
+<h2><?=$_data["uye_adsoyad"]?></h2>
+<strong> Gmail: </strong><?=$_data["uye_eposta"]?>
 <hr>
 
 <table border = "1" width = 100% >
@@ -17,31 +27,57 @@ include 'fonksiyon.php';
 		<td>
 			<strong> Yeni Açılan Konular: </strong>
 			<ul>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-			</ul>
+				<?php
+				$dataList = $db -> prepare("SELECT * FROM konular WHERE konu_uye_id=?");
+				$dataList -> execute([
+					$_data["uye_id"]
+				]);
+				$dataList = $dataList -> fetchALL(PDO::FETCH_ASSOC);
+				
+				foreach($dataList as $row){
+					echo '<li><a href="konu.php?link='.$row["konu_link"].'">'.$row["konu_ad"].'</a></li>';
+				}
+				?>
+				</ul>
 		</td>
 		<td>
 			<strong> Son Cevaplar: </strong><hr>
 			<ul>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
+			<?php
+				$dataList = $db -> prepare("SELECT * FROM yorumlar WHERE y_uye_id=? LIMIT 100");
+				$dataList -> execute([
+					$_data["uye_id"]
+				]);
+				$dataList = $dataList -> fetchALL(PDO::FETCH_ASSOC);
+				
+				$konu_idler = [];
+
+				foreach($dataList as $row){
+					array_push( $konu_idler, $row["y_konu_id"] );
+
+				}
+
+				$konu_idler = array_unique( $konu_idler );
+
+				foreach($konu_idler as $konu_id) {
+
+$konu_cek = $db -> prepare("SELECT * FROM konular WHERE
+  konu_id=?
+");
+$konu_cek -> execute([
+  $konu_id
+]);
+$konucek = $konu_cek -> fetch(PDO::FETCH_ASSOC);
+
+echo '<li><a href="konu.php?link='.$konucek["konu_link"].'">'.$konucek["konu_ad"].'</a></li>';
+@$i++;
+					if ($i == 10)
+					{
+						break;
+					}
+
+				}
+				?>
 			</ul>
 		</td>
 	</tr>
