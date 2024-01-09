72b27d3231be28866a285959e477f518c53e461c
Serkan <224212041@ogrenci.amasya.edu.tr>
2024-01-09 20:14:50 +0300

    Konu <> v1_3
    Kayıtlı kullanıcının konuya yorum yapabilmesi yapıldı.

--- konu.php
+++ konu.php
@@ -40,10 +40,69 @@ $data = $db -> prepare("SELECT * FROM konular WHERE
 <p>
     <small><?=$_data["konu_tarih"]?></small>
 </p>
+<hr>
+<p>Yorumlar: </p>
+<?php
 
+$dataList = $db -> prepare("SELECT * FROM yorumlar WHERE y_konu_id=? ORDER BY y_id DESC");
+$dataList -> execute([
+  $_data["konu_id"]
+]);
+$dataList = $dataList -> fetchALL(PDO::FETCH_ASSOC);
+
+foreach($dataList as $row){
+    echo '<a href="profil.php?kadi='.uye_ID_to_kadi($row["y_uye_id"]).'" id=yorum"id'.$row["y_id"].'><strong>'
+    .uye_ID_to_isim($row["y_uye_id"]).'</strong></a><br>
+    <p>
+    '.$row["y_yorum"].'
+    </p>
+    <small><strong>Tarih: </strong>'.$row["y_tarih"].'</small>
+    <hr>';
+
+}
+?>
 <?php
 if (@$_SESSION["uye_id"]) {
-  echo '<h4>Konuya Yorum Yap: </h4>
+
+  if ($_POST)
+  {
+    $yorum = $_POST["yorum"];
+
+    $dataAdd = $db -> prepare("INSERT INTO yorumlar SET
+    y_uye_id=?,
+    y_konu_id=?,
+    y_yorum=?
+");
+$dataAdd -> execute([
+    $_SESSION["uye_id"],
+    $_data["konu_id"],
+    $yorum
+]);
+
+if ( $dataAdd ) {
+
+  $yorumcek = $db -> prepare("SELECT * FROM yorumlar WHERE
+  y_uye_id=?
+  &&
+  y_konu_id=?
+");
+$yorumcek -> execute([
+  $_SESSION["uye_id"],
+    $_data["konu_id"]
+]);
+$yorum_cek = $yorumcek -> fetch(PDO::FETCH_ASSOC);
+
+    echo '<p class="alert alert-success">Yorum eklendi.</p>';
+    
+    // Yönlendirme yapar.
+    header("REFRESH:1;URL=konu.php?link=" . $link . "#yorum" . $yorum_cek["y_id"]);
+} else {
+    echo '<p class="alert alert-danger">Konunuz eklenirken hata oluştu.</p>';
+    
+    header("REFRESH:1;URL=konu.php?link=" . $link . "#yorumyap");
+}
+  }
+  echo '<h4 id="yorumyap">Konuya Yorum Yap: </h4>
   <form action="" method="post">
   <textarea name="yorum" cols="30" rows="15"> </textarea>
   <br>
