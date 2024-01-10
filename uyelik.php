f550236cb75778a68d92914e542edb71d5cb17b5
Serkan <224212041@ogrenci.amasya.edu.tr>
2024-01-10 08:10:51 +0300

    Üyelik <> v1_2
    Unutulan şifreyi güncelleme kaldırıldı.

--- uyelik.php
+++ uyelik.php
@@ -27,71 +27,8 @@
                         if($selectRow > 0){ // Var
                             $yeniSifre  = time() . rand(111,999);
                             $Sifrele    = md5(sha1( $yeniSifre ));
-
-                            $sifreyiGuncelle = $db->prepare("UPDATE uyeler SET uye_sifre=? WHERE uye_eposta=?");
-                            $sifreyiGuncelle -> execute([
-                                $Sifrele,
-                                $eposta
-                            ]);
-
-                            if ($sifreyiGuncelle) {
-
-
-
-
-
-                                require("emailotomasyonu/class.phpmailer.php");
-                                
-                                $mailim = "yin_sa0339@gmail.com";
-                                $mail = new PHPMailer();
-                                $mail->IsSMTP(true);
-                                $mail->CharSet = 'UTF-8';
-                                $mail->From     = $mailim;
-                                $mail->Sender   = $mailim;
-                                $mail->FromName = "Serkan Mutlu";
-                                $mail->Host     = "yin_sa0339@gmail.com";
-                                $mail->SMTPAuth = true;
-                                $mail->SMTPSecure = false;
-                                $mail->SMTPAutoTLS = false;
-                                $mail->Port     = 587;
-                                
-                                $mail->Username = "yin_sa0339@gmail.com.com";
-                                $mail->Password = 'şifreniz';
-                                $mail->WordWrap = 50;
-                                $mail->IsHTML(true);
-                                $mail->Subject  = "Başlık";
-                                $body  = '<b>Site adınız:</b><br>Şifreniz yenilendi! Yeni şifreniz: ' . $yeniSifre . '<br><b>Not:</b> Eğer şifrenizi siz güncellemediyseniz lütfen info@site.com adresine eposta gönderiniz!<br><br><b>Tarih:</b> ' . date("m.d.Y") . ' <b>Saat:</b> ' . date("H:i:s");
-                                
-                                $textBody = $body;
-                                $mail->Body = $body;
-                                $mail->AltBody = $textBody;
-                                $mail->ClearAddresses();
-                                $mail->ClearAttachments();
-                                
-                                $mail->AddBCC( $eposta );
-                                
-                                $mail->Send();
-                                $mail->ClearAddresses();
-                                $mail->ClearAttachments();
-                                error_reporting(0);
-                                
-
-
-
-
-
-
-
-                                echo '<p class="alert alert-success">Şifreniz başarıyla güncellendi! Lütfen eposta adresinizi kontrol ediniz. Yeni şifrenizi oraya gönderdik! :) [NOT: Spam kutunuza da bakmayı unutmayınız!]</p>';
-
-
-                            } else {
-                                echo '<p class="alert alert-danger">Şifreniz güncellenemedi! Lütfen tekrar deneyiniz!</p>';
-                            }
-                        }else{ // Yok
-                            echo '<p class="alert alert-danger">Böyle bir eposta adresi bulunmamaktadır!</p>';
                         }
-                    } else { // :(
+                    } else {
                         echo '<p class="alert alert-danger">Lütfen gerçek bir eposta adresi yazınız!</p>';
                     }
                 }
@@ -101,12 +38,10 @@
             <div class="container mt-5">
                 <div class="row justify-content-center">
                     <div class="col-lg-6 text-center">
-                        <h1 class="mb-5 d-block">Şifremi Unuttum</h1>
                         <form action="" method="post">
                             <strong>Eposta:</strong>
                             <input type="email" name="eposta" class="form-kontrol" placeholder="sakip@sabanci.com">
-                            <input type="submit" name="sifremiunuttum" value="Yeni Şifremi Epostama Gönder" class="mavi-menu mt-3">
-                        </form>
+                            </form>
                         <a href="uyelik" class="d-block mt-2">Giriş Yap</a>
                     </div>
                 </div>
@@ -160,8 +95,6 @@
                         <input type="password" class="form-control" name="sifre"><br />
                         <input type="submit" class="btn btn-block btn-dark" name="giris" value="Giriş Yap">
                     </form>
-                    
-                    <a href="uyelik.php?p=sifremiunuttum" class="btn btn-block btn-secondary mt-3">Şifremi unuttum</a><br>
                     <a href="uyelik.php?p=kayit" class="btn btn-block btn-secondary mt-3">Şimdi kayıt ol!</a>
                     <hr>
                     <a href="index.php" class="text-dark"><small>&larr; Anasayfaya dön</small></a>';
