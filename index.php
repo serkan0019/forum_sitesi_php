73114f0d5f14885a451cb1dfd451d46f87fb739f
Serkan <224212041@ogrenci.amasya.edu.tr>
2024-01-10 07:35:53 +0300

    Ana Sayfa <> v1_3
    Ana sayfa da açılan konuların görüntülenmesi, yazılan son cevapların görüntülenmesi, kategorilerin görüntülenmesi yapıldı.

--- index.php
+++ index.php
@@ -15,31 +15,50 @@ include 'fonksiyon.php';
 		<td>
 			<strong> Yeni Açılan Konular: </strong><hr>
 			<ul>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
+			<?php
+			$dataList = $db -> prepare("SELECT * FROM konular ORDER BY konu_id DESC LIMIT 10");
+			$dataList -> execute();
+			$dataList = $dataList -> fetchALL(PDO::FETCH_ASSOC);
+			foreach($dataList as $row){
+			echo '<li><a href=konu.php?link='.$row["konu_link"].'>'.$row["konu_ad"].'</a></li>';
+		}
+			?>
 			</ul>
 		</td>
 		<td>
 			<strong> Son Cevaplar: </strong><hr>
 			<ul>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
+			<?php
+				$dataList = $db -> prepare("SELECT * FROM yorumlar ORDER BY y_id DESC LIMIT 100");
+				$dataList -> execute();
+				$dataList = $dataList -> fetchALL(PDO::FETCH_ASSOC);
+				
+				$konu_idler = [];
+
+				foreach($dataList as $row){
+					array_push( $konu_idler, $row["y_konu_id"] );
+
+				}
+
+				$konu_idler = array_unique( $konu_idler );
+
+				foreach($konu_idler as $konu_id) {
+					$konu_cek = $db -> prepare("SELECT * FROM konular WHERE
+					konu_id=?
+					");
+					$konu_cek -> execute([
+						$konu_id
+					]);
+					$konucek = $konu_cek -> fetch(PDO::FETCH_ASSOC);
+					echo '<li><a href="konu.php?link='.$konucek["konu_link"].'">'.$konucek["konu_ad"].'</a></li>';
+					@$i++;
+					if ($i == 10)
+					{
+						break;
+					}
+
+				}
+				?>
 			</ul>
 		</td>
 	</tr>
@@ -48,16 +67,14 @@ include 'fonksiyon.php';
 			<h2> Kategoriler: </h2>
 
 			<ul>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
-				<li><a href=""> Konu </a></li>
+			<?php
+			$dataList = $db -> prepare("SELECT * FROM kategoriler LIMIT 10");
+			$dataList -> execute();
+			$dataList = $dataList -> fetchALL(PDO::FETCH_ASSOC);
+			foreach($dataList as $row){
+				echo '<li><a href="kategori.php?link='.$row["k_kategori_link"].'">'.$row["k_kategori"].'</a></li>';
+			}
+			?>
 			</ul>
 		</td>
 	</tr>
